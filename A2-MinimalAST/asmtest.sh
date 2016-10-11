6f4e4f870595e9e43bf76f6ef1e8c7f41d922f08
Erik Bjäreholt <erik.bjareholt@gmail.com>
2016-10-11 20:06:53 +0200

    We can now compile something that returns, yay

---
+++ A2-MinimalAST/asmtest.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+ant &&
+java -cp "../lib/beaver-rt.jar:ant-bin" lang.ActualCompiler testfiles/assembly/1.c > testfiles/assembly/1.s &&
+as testfiles/assembly/1.s -o testfiles/assembly/1.o &&
+ld testfiles/assembly/1.o -o testfiles/assembly/1.elf
